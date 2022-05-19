424bbd243eee2964efd909766d96bcec19662e26
Paulo <mudeideideia@gmail.com>
2022-05-18 22:59:37 -0300

    Incluindo palavras novas.

--- dev_test/script_table_vocab.sh
+++ dev_test/script_table_vocab.sh
@@ -4,3 +4,4 @@ DROP TABLE IF EXISTS vocab;
 CREATE TABLE vocab("word" TEXT, "hiragana" TEXT, "furigana" TEXT, "portuguese" TEXT, "english" TEXT);
 .import --csv "./vocab.csv" vocab
 END_SQL
+cp japanese.sqlite3 ../database/japanese.sqlite3
--- public/index.js
+++ public/index.js
@@ -4,7 +4,8 @@ const url = 'http://10.0.0.169:3000'
 const lyrics_space = document.getElementById("lyrics-space")
 const hiragana_space = document.getElementById("hiragana")
 const portuguese_space = document.getElementById("portuguese")
-lyrics_space.innerHTML = "仮面ライダーBLACK<br><br><br>時を超えろ 空を駆けろ この星のため<br>君は見たか 愛が真っ紅に燃えるのを<br>暗い闇の底で 危険な罠が待つ<br><br>信じる奴が 正義(ジャスティス)<br>真実の王者<br>夢を見続ける事が 俺のファンタジー<br>生きることが好きさ 蒼く浮かぶ宇宙<br>時を超えろ 空を駆けろ この星のため<br>熱く燃やせ 涙 流せ 明日という日に<br>仮面ライダー BLACK<br>仮面ライダー BLACK<br><br>黒く光るボディ ハートに血が通う<br>風が運ぶ歌に気持ちがふと揺れる<br><br>支配したがる魔術師(マジシャン)<br><br>妖しげな超能力<br><br>闘う時は 戦士(ソルジャー) 俺の誇りさ<br>この地球が好きさ 心許した友<br><br>永遠に守れ 若さ弾け この愛のため<br><br>現在を燃やせ 強く生きろ 今日という日を<br>仮面ライダー BLACK<br>仮面ライダー BLACK<br><br>信じる奴が 正義(ジャスティス)<br>真実の王者<br>夢を見続ける事が 俺のファンタジー<br>生きることが好きさ 蒼く浮かぶ宇宙<br>時を超えろ 空を駆けろ この星のため<br>熱く燃やせ  涙  流せ 明日という日に<br>仮面ライダー BLACK<br>仮面ライダー BLACK";
+
+lyrics_space.innerHTML = "仮面ライダーBLACK<br><br><br>時を超えろ 空を駆けろ<br>この星のため<br><br>君は見たか愛が<br>真っ紅に燃えるのを<br>暗い闇の底で<br>危険な罠が待つ<br><br>信じる奴が正義（ジャスティス）<br>真実の王者<br>夢を見続ける事が<br>俺のファンタジー<br><br>生きることが好きさ<br>蒼く浮かぶ宇宙（コスモ）<br><br>時を超えろ 空を駆けろ<br>この星のため<br>熱く燃やせ<br>涙流せ<br>明日という日に<br>仮面ライダー BLACK<br>仮面ライダー BLACK<br><br>黒く光るボディ<br>ハートに血が通う<br>風が運ぶ歌に<br>気持ちがふと揺れる<br><br>支配したがる魔術師（マジシャン）<br>妖しげな超能力（エスパー）<br>闘う時は戦士（ソルジャー）<br>俺の誇りさ<br><br>この地球が好きさ<br>心許した友<br><br>永遠に守れ <br>若さ弾け<br>この愛のため<br>現在を燃やせ<br>強く生きろ<br>今日という日を<br>仮面ライダー BLACK<br>仮面ライダー BLACK<br><br>信じる奴が正義（ジャスティス）<br>真実の王者<br>夢を見続ける事が<br>俺のファンタジー<br><br>生きることが好きさ<br>蒼く浮かぶ宇宙（コスモ）<br><br>時を超えろ 空を駆けろ<br>この星のため<br>熱く燃やせ<br>涙流せ<br>明日という日に<br>仮面ライダー BLACK<br>仮面ライダー BLACK";
 let vocabs;
 
 const onOver = (e)=>{
