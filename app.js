a932c3c06ac14a94584cc822c6649db76be10ac4
mudeideideia <44714407+mudeideideia@users.noreply.github.com>
2022-05-19 20:05:56 -0300

    Preparando pra integrar com Heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = 3000
+const port = process.env.PORT || 3000
 
 const db = require('./database/database');
 const songs = require('./songs');
